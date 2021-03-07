d06bfd497461ac0c79625e0ba249c1730c43b3ce
oi <onno.invernizzi@ivention.nl>
2021-03-07 09:07:38 +0100

    Intermediate works!

--- src/Backend/RegisterICPMainCalls.ts
+++ src/Backend/RegisterICPMainCalls.ts
@@ -2,6 +2,7 @@ import { ipcMain } from "electron";
 import { Channels, IIntermediate } from "../Typings";
 import { Intermediate } from "./Intermediate";
 
+// Register 'Succes' call.
 registerIcpOn("Succes", "succes");
 
 function registerIcpOn(channel: Channels, handler: keyof IIntermediate): void {
@@ -10,7 +11,11 @@ function registerIcpOn(channel: Channels, handler: keyof IIntermediate): void {
         // Cast to any so we can spread args in the method call. This is save, the arguments
         // and return type are all typed and expected.
         const method = Intermediate[handler] as any;
-        const response = method(...args);
-        event.sender.send(channel, response);
+
+        // Cannot use await here so lets use a then. No need for a catch, we're using a response model
+        // and if its succes flag is false the we handle an error.
+        method(...args).then((response: any) => {
+            event.sender.send(channel, response);
+        });
     });
 }
